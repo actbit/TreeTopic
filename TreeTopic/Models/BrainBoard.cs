745633e8ee76714e8d3f131c6c189868af866e42
actbit <57023457+actbit@users.noreply.github.com>
2025-12-18 23:20:40 +0900

    [add] model

---
+++ TreeTopic/Models/BrainBoard.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TreeTopic.Models
+{
+    public class BrainBoard : BaseModel
+    {
+        [ForeignKey(nameof(Topic))]
+        public Guid TopicId { get; set; }
+        public Topic Topic { get; set; }
+        public string Name { get; set; }
+        public bool IsSign { get; set; }
+    }
+}
